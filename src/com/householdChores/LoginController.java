4ee4bad39a127ccf837003df7702b7a24e2e9406
psogani <psogani@indiana.edu>
2014-11-04 15:11:34 -0500

    Working on Junit

--- src/com/householdChores/LoginController.java
+++ src/com/householdChores/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
-
+	
 	@RequestMapping(value="/Login", method = RequestMethod.GET)
 	public ModelAndView getLoginData(ModelAndView m) 
 	{
