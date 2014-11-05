83cd4a962fafef8c594825478ec3d071ad86230a
Abhishek Datar <adatar12@gmail.com>
2014-11-05 16:21:56 -0500

    DBConnect junit, and updated classpath for junit.

---
+++ src/com/junit/householdChores/DBConnectTest.java
@@ -0,0 +1,17 @@
+package com.junit.householdChores;
+
+import static org.junit.Assert.*;
+import com.householdChores.JDBCConnection;
+import org.junit.Test;
+
+public class DBConnectTest {
+
+	JDBCConnection jdbcConnection = new JDBCConnection();
+
+	@Test
+	public void dbConectionTest() {
+		
+		assertNotNull(jdbcConnection.makeConnection());
+	}
+
+}
